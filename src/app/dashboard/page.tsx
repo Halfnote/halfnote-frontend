42a94919a9b4a6ba7338b6583f5b5181224cc440
Oditha <oditha14@gmail.com>
2025-04-08 19:29:09 -0400

    theming fonts

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+export default function Dashboard() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="another-heading1">Heading 1</h1>
+        <p className="text-center another-heading2">
+          This is the dashboard page.
+        </p>
+      </main>
+    </div>
+  );
+}
