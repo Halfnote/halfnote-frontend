f245e8d74610531225c4f66c57e23afb07c4a86a
Oditha <oditha14@gmail.com>
2025-04-08 20:52:02 -0400

    iconography

---
+++ src/app/icons/icons.tsx
@@ -0,0 +1,21 @@
+import Classical from "./Classical_Genre_Tag.svg";
+import Country from "./Country_Genre_Tag.svg";
+import Electronic from "./Electronic_Genre_Tag.svg";
+import Funk from "./Funk_Genre_Tag.svg";
+import Gospel from "./Gospel_Genre_Tag.svg";
+import HipHop from "./HipHop_Genre_Tag.svg";
+import Jazz from "./Jazz_Genre_Tag.svg";
+import Pop from "./Pop_Genre_Tag.svg";
+import Rock from "./Rock_Genre_Tag.svg";
+
+export const Icons = {
+  classical: Classical,
+  country: Country,
+  electronic: Electronic,
+  funk: Funk,
+  gospel: Gospel,
+  hiphop: HipHop,
+  jazz: Jazz,
+  pop: Pop,
+  rock: Rock,
+};
