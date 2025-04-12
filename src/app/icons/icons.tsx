53f44aecb9e708cb656a5b88ee9aa2f5ca8f67ad
Oditha <oditha14@gmail.com>
2025-04-12 14:59:24 -0400

    added all color icons and fixed dashboard

--- src/app/icons/icons.tsx
+++ src/app/icons/icons.tsx
@@ -1,12 +1,17 @@
-import Classical from "./Classical_Genre_Tag.svg";
-import Country from "./Country_Genre_Tag.svg";
-import Electronic from "./Electronic_Genre_Tag.svg";
-import Funk from "./Funk_Genre_Tag.svg";
-import Gospel from "./Gospel_Genre_Tag.svg";
-import HipHop from "./HipHop_Genre_Tag.svg";
-import Jazz from "./Jazz_Genre_Tag.svg";
-import Pop from "./Pop_Genre_Tag.svg";
-import Rock from "./Rock_Genre_Tag.svg";
+import Classical from "@/app/icons/another_col_icons/CLASSICAL-genreTag.svg";
+import Country from "@/app/icons/another_col_icons/COUNTRY-genreTag.svg";
+import Electronic from "@/app/icons/another_col_icons/ELECTRONIC-genreTag.svg";
+import Funk from "@/app/icons/another_col_icons/FUNK-genreTag.svg";
+import Gospel from "@/app/icons/another_col_icons/GOSPEL-genreTag.svg";
+import HipHop from "@/app/icons/another_col_icons/HIPHOP-genreTag.svg";
+import Jazz from "@/app/icons/another_col_icons/JAZZ-genreTag.svg";
+import Pop from "@/app/icons/another_col_icons/POP-genreTag.svg";
+import Rock from "@/app/icons/another_col_icons/ROCK-genreTag.svg";
+import Reggae from "@/app/icons/another_col_icons/REGGAE-genreTag.svg";
+import Soundtrack from "@/app/icons/another_col_icons/SOUNDTRACK-genreTag.svg";
+import World from "@/app/icons/another_col_icons/WORLD-genreTag.svg";
+import Latin from "@/app/icons/another_col_icons/LATIN-genreTag.svg";
+import Folk from "@/app/icons/another_col_icons/FOLK-genreTag.svg";
 
 export const Icons = {
   classical: Classical,
@@ -18,4 +23,9 @@ export const Icons = {
   jazz: Jazz,
   pop: Pop,
   rock: Rock,
+  reggae: Reggae,
+  soundtrack: Soundtrack,
+  world: World,
+  latin: Latin,
+  folk: Folk,
 };
