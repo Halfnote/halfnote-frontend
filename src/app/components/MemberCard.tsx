32495f60b3de78db51de80cb47b05940810448a2
Oditha <oditha14@gmail.com>
2025-05-10 20:52:38 -0700

    member card and grid fix

--- src/app/components/MemberCard.tsx
+++ src/app/components/MemberCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Image, { StaticImageData } from "next/image";
 
+import { Icons } from "../icons/icons";
 type MemberCardProps = {
   numRatings: number;
-  profilePic: string;
-  topAlbums: Array<string>;
+  profilePic: StaticImageData;
+  topAlbums: Array<StaticImageData | string>;
   userName: string;
 };
 
@@ -14,23 +16,70 @@ export const MemberCard = ({
   userName,
 }: MemberCardProps) => {
   return (
-    <div className="flex flex-col w-full border-2 border-[#9A9A9A] rounded-xl p-5 mb-4">
-      <div className="flex flex-row gap-5 w-full">
+    <div className="flex flex-col border-2 border-[#9A9A9A] rounded-xl w-[212px] h-[212px] pl-6 pr-6 pt-3 mb-4">
+      <div className="flex flex-row gap-10 w-full items-center">
         {/* {first} */}
-        <div className="flex flex-col min-w-0">
-          <h1>pic</h1>
-          <h1 className="truncate">username</h1>
+        <div className="flex flex-col min-w-0 items-center">
+          <Image
+            src={profilePic}
+            alt="Profile Picture"
+            className="aspect-square w-[75px] object-cover rounded-full ring-2"
+          />
+
+          <h1 className="truncate another-heading3">{userName}</h1>
         </div>
-        <div className="flex flex-col min-w-0">
-          <h1>vinyl</h1>
-          <h1>rating</h1>
-          <h3>reviews</h3>
+        <div className="flex flex-col min-w-0 items-center gap-0">
+          {numRatings === 0 ? (
+            <Image
+              src={Icons.firstVinyl}
+              alt="Profile Picture"
+              className="aspect-square w-[54px] object-cover rounded-full ring-2"
+            />
+          ) : numRatings >= 1 && numRatings <= 99 ? (
+            <Image
+              src={Icons.secondVinyl}
+              alt="Profile Picture"
+              className="aspect-square w-[54px] object-cover rounded-full ring-2"
+            />
+          ) : numRatings >= 100 && numRatings <= 499 ? (
+            <Image
+              src={Icons.thirdVinyl}
+              alt="Profile Picture"
+              className="aspect-square w-[54px] object-cover rounded-full ring-2"
+            />
+          ) : numRatings >= 500 && numRatings <= 1499 ? (
+            <Image
+              src={Icons.fourthVinyl}
+              alt="Profile Picture"
+              className="aspect-square w-[54px] object-cover rounded-full ring-2"
+            />
+          ) : (
+            <Image
+              src={Icons.fifthVinyl}
+              alt="Profile Picture"
+              className="aspect-square w-[54px] object-cover rounded-full ring-2"
+            />
+          )}
+          <h1 className="another-heading4 -mb-1">{numRatings}</h1>
+          <h3 className="another-body mt-0">reviews</h3>
         </div>
       </div>
-      <div className="flex flex-row gap-4 w-full">
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
+      <div className="flex flex-row gap-1 justify-center mt-3">
+        <Image
+          src={topAlbums[0]}
+          alt="Top Album"
+          className="w-[60px] h-[60px] object-cover rounded-md object-center"
+        />
+        <Image
+          src={topAlbums[1]}
+          alt="Top Album"
+          className="w-[60px] h-[60px] object-cover rounded-md object-center"
+        />
+        <Image
+          src={topAlbums[2]}
+          alt="Top Album"
+          className="w-[60px] h-[60px] object-cover rounded-md object-center"
+        />
       </div>
     </div>
   );
