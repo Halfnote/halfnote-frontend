6636f5bc15f74e599ee1f7a86297d27deb5d1da5
Oditha <oditha14@gmail.com>
2025-05-17 14:39:20 -0700

    fixed formatting issue for new release carousel

--- src/app/components/NewReleaseCarousel.tsx
+++ src/app/components/NewReleaseCarousel.tsx
@@ -5,6 +5,7 @@ import { EffectCoverflow, Pagination } from "swiper/modules";
 import { NewReleases } from "../types/review";
 import Image from "next/image";
 import Clairo from "../../../public/sample_images/813adHqElhL.jpg";
+import { SwiperOptions } from "swiper/types";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -15,32 +16,50 @@ type ListCarouselProps = {
 
 export const NewReleasesCarousel = ({ items }: ListCarouselProps) => {
   return (
-    <Swiper
-      modules={[EffectCoverflow]}
-      effect={"coverflow"}
-      loop={true}
-      spaceBetween={20}
-      slidesPerView={7}
-      pagination={{ clickable: true }}
-      centeredSlides={true}
-      grabCursor={true}
-      coverflowEffect={{ rotate: 0, slideShadows: false }}
-      className="w-full h-auto"
-    >
-      {items.map((item, index) => (
-        <SwiperSlide key={index} className="flex items-center justify-center">
-          {({ isActive }) => {
-            <div className="relative w-[245px] h-[245px]">
-              <Image
-                src={Clairo}
-                alt={item.albumName}
-                fill
-                className="object-cover border-2 border-black"
-              />
-            </div>;
-          }}
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="w-full flex justify-center items-center">
+      <Swiper
+        modules={[EffectCoverflow]}
+        effect={"coverflow"}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={7}
+        pagination={{ clickable: true }}
+        centeredSlides={true}
+        grabCursor={true}
+        coverflowEffect={{
+          rotate: 15,
+          slideShadows: false,
+          depth: 200,
+          stretch: 2,
+        }}
+        className="w-[90%]"
+      >
+        {items.map((item, index) => (
+          <SwiperSlide key={index} className="flex items-center justify-center">
+            {({ isActive }) => (
+              <div className="relative w-[245px] h-full">
+                <Image
+                  src={Clairo}
+                  alt={item.albumName}
+                  className={`object-cover border-2 ${
+                    isActive ? "border-red-400" : "border-black"
+                  }`}
+                />
+                {isActive && (
+                  <div className="w-full text-center">
+                    <h1 className="another-top-rated-album whitespace-nowrap">
+                      {item.albumName}
+                    </h1>
+                    <h2 className="another-heading4 whitespace-nowrap">
+                      {item.artistName}
+                    </h2>
+                  </div>
+                )}
+              </div>
+            )}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
