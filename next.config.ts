ae19895fc7ace7d6075c8765656af29eb25fe077
Oditha <oditha14@gmail.com>
2025-10-12 00:14:46 -0400

    removed serverAction line from config

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "i.discogs.com", "st.discogs.com"],
   },
-  serverActions: {
-    bodySizeLimit: "10mb",
-  },
 };
 
 export default nextConfig;
